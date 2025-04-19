71cd8922431f23c5e111f8653cbf1601d96ef03f
goodluckjw <chetera@naver.com>
2025-04-19 19:23:33 +0900

    Update law_editor_app.py

--- 042.001/app/law_editor_app.py
+++ 042.001/app/law_editor_app.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
-from processing.law_processor import get_law_list_from_api, get_highlighted_articles
+from law_processor import get_law_list_from_api, get_highlighted_articles
 
 st.set_page_config(page_title="📘 부칙 개정 도우미")
 st.title("📘 부칙 개정 도우미")
